108dfd29a9418d9d1d4f334e6a45bb311956d4da
farukyucel5 <farukyucel5@gmail.com>
2023-08-02 14:57:52 +0300

    Updated

---
+++ BaseClassConstructor/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace BaseClassConstructor
+{
+    internal class MainClass
+    {
+
+        static void Main(string[] args)
+        {
+            ChildClass childClass = new("Product A");
+            childClass.Add();
+
+        }
+    }
+    internal class BaseClass
+    {
+        private readonly string _entity;
+
+        public BaseClass(string entity)
+        {
+
+            _entity = entity;
+        }
+
+        public void Message()
+        {
+            Console.WriteLine($"{_entity} message");
+        }
+
+
+    }
+
+    internal class ChildClass : BaseClass
+    {
+        public ChildClass(string entity) : base(entity)
+        {
+
+        }
+
+        public void Add()
+        {
+
+            Console.WriteLine("Added");
+            Message();
+        }
+    }
+
+
+}
