21941492fd45ea17843670de6aa7c75d2bdd791f
farukyucel5 <farukyucel5@gmail.com>
2023-08-03 18:55:50 +0300

    Entity framework first query

---
+++ EntityFrameworkIntro/CitiesDal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkIntro
+{
+    internal class CitiesDal
+    {
+        public static List<City> GetAllCities()
+        {
+            using WorldContext context = new();
+            return context.Cities.ToList();
+        }
+
+        public static void Add(City city)
+        {
+            using WorldContext context = new();
+            context.Cities.Add(city);
+            context.SaveChanges();
+        }
+
+
+    }
+}
---
+++ EntityFrameworkIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using EntityFrameworkIntro;
+using Microsoft.EntityFrameworkCore;
+
+
+List<City> list = new List<City>();
+
+list = CitiesDal.GetAllCities();
+list.ForEach(p => Console.WriteLine(p.CityName.ToString()));
+
+CitiesDal.Add(new City()
+{
+    CityName = "Erzincan",
+    Population = 4000000,
+    Latitude = 39.920800M,
+    Longitude = 39.920800M,
+    Region = "Eastern",
+    Country = "Turkey"
+});
+Console.WriteLine("===============updated=================");
+list = CitiesDal.GetAllCities();
+list.ForEach(p => Console.WriteLine(p.CityName.ToString()));
+
+
+
